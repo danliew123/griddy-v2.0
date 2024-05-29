c6475316f7a20b9e4dee81f6c3242943d0822a07
danliew123 <169648609+danliew123@users.noreply.github.com>
2024-05-29 16:50:21 +0800

    Add drawing and change grid functions
    erase button can't get it to work

--- javascript.js
+++ javascript.js
@@ -8,10 +8,60 @@ for (let i = 0; i < 16; i++) {
         square.style.border = '1px solid black';
         square.style.boxSizing = 'border-box'
         square.style.margin = '0px'
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+            console.log('touch')
+        })
         container.appendChild(square);
     }
 }
 
-for (let i = 0; i < 5; i++) {
-    console.log('wtf');
-}
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', () => {
+    const size = prompt('how griddy u want it?');
+    const squares = document.querySelectorAll('div.container > div');
+    squares.forEach(e => e.remove());
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++){
+            const square = document.createElement('div');
+            square.style.width = `${512 / size}px`;
+            square.style.height = `${512 / size}px`;
+            square.style.border = '1px solid black';
+            square.style.boxSizing = 'border-box'
+            square.style.margin = '0px'
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'black';
+                console.log('touch')
+            })
+            container.appendChild(square);
+        }
+    }
+})
+
+const squares = document.querySelectorAll('div.container > div');
+const erase = document.querySelector('.erase')
+
+erase.addEventListener('click', () => {
+    console.log('erase')
+    squares.forEach(e => {
+        e.removeEventListener('mouseover', () => {
+            e.style.backgroundColor = 'black';
+            console.log('touch')
+        })
+
+        e.addEventListener('mouseover', () => {
+            e.style.backgroundColor = '';
+            console.log('touch')
+        })
+    })
+})
+
+const draw = document.addEventListener('click', () => {
+    squares.forEach(e => {
+        e.addEventListener('mouseover', () => {
+            e.style.backgroundColor = 'black';
+            console.log('touch')
+        })
+    })
+})
+
